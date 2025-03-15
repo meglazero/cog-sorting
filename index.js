2caf80368cc5c6c110eca918a130155163410c78
Butt <alexander.vanmeter@gmail.com>
2025-03-15 15:05:04 -0500

    Added base files, and some info in javascript related to deciphering the json for what I'm messing with

---
+++ index.js
@@ -0,0 +1,38 @@
+/*const node = document.createElement("p");
+const textnode = document.createTextNode("Potato");
+
+node.appendChild(textnode);
+document.getElementsByClassName("standard")[0].appendChild(node);*/
+
+/* Cog Info Dump
+"CogM": "{\"0\":{\"d\":147},\"1\":{\"d\":111,\"a\":1166},\"2\":{\"d\":110,\"a\":284}}
+0-95 is cogs, 96-107 is characters sitting in cog shelf, 108+ is created cogs sitting in shelf
+for characters; a: build rate, b: exp/hr, c: flaggy/hr
+cogs:
+    h: directional
+        left, right, up, down: (le,ri,up,do) normal directional, obvious
+        row/column: (ro,co) obvious
+        around: (Y) gem shop cogs, small
+        corners: (cr) diagonal directional
+        adjacent: (ad) + directional
+        everything: obvious, from excogia only
+        diagonal: (di) + turned 45 degrees
+    a: build rate
+    c: flaggy rate
+    d: bonus construct exp
+    e: directional build rate
+    f: directional exp bonus
+    l: not really sure, it is only on the p2w excogia cogs though, whether connected or not
+
+
+    "CogO": "[\"Cog2A0\",\"Cog3A00\",\"Cog2A00\",\"Cog3A00\",\"Cog3B0\",\"Cog3co\",\"Cog3B1\"]
+    presumably CogO[0]-CogO[95] is cog board, CogO[96]-CogO[107] is cog shelf creators
+    and CogO[108]-CogO[CogO.length()-1] is created cogs
+    Cog[0-3][a-c][00-5]
+    Nooby CogO, Decent Cog1, Superb Cog2, Ulti Cog3
+    Cog(Broken): A00, Cog: A0, Average: A1, Spur: A2, Deckered A3
+    Double: B0, Trips: B1, Trabble: B2, Quad: B3, Penta: B4
+    Ulti Broken Cog: Cog3A00
+    Ulti Double Cog: Cog3B0
+    Ulti Left Directional Cog: Cog3le
+*/
